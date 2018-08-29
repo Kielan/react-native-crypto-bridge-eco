b5f50c74a0361ad0ebf68ee40faec701b261713a
Kielan <ki.lemons@gmail.com>
2018-08-29 04:11:48 -0700

    export first method createhmac and test in js

--- ios/RNReactNativeCryptoBridge.m
+++ ios/RNReactNativeCryptoBridge.m
@@ -7,19 +7,7 @@
 
 #import <Foundation/Foundation.h>
 
-#if __has_include(<React/RCTBridgeModule.h>)
-#import <React/RCTBridgeModule.h>
-#elif __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
-#import "React/RCTBridgeModule.h"
-#endif
-#import "RNReactNativeCryptoBridge.h"
-
 //@interface RCT_EXTERN_MODULE(RNGoogleSignIn, NSObject)
 
 // RCT_EXTERN_METHOD(addEvent:(NSString *)name location:(NSString *)location date:(nonnull NSNumber *)date callback: (RCTResponseSenderBlock)callback);
-RCT_EXTERN_METHOD(createHmac(NSDictionary *)config);
-
-@end
-
+RCT_EXTERN_METHOD(createHmac(NSString *), key:(NSString *))
